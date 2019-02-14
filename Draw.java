45e2589bf16cfd8cc68b11118ca9aaacb41f9df4
buttch33k <nlacsamana09@gmail.com>
2019-02-14 11:42:49 +0800

    seperate Player object

--- Draw.java
+++ Draw.java
@@ -12,10 +12,10 @@ public class Draw extends JComponent{
 	private BufferedImage image;
 	private BufferedImage backgroundImage;
 	public URL resource = getClass().getResource("run0.png");
+	public Player player;
 
 	// circle's position
-	public int x = 30;
-	public int y = 30;
+	
 
 	// animation states
 	public int state = 0;
@@ -32,6 +32,7 @@ public class Draw extends JComponent{
 		monster3 = new Monster(400, 300);
 		monster4 = new Monster(500, 200);
 		monster5 = new Monster(100, 400);
+		player = new Player(30, 30, this);
 
 		try{
 			image = ImageIO.read(resource);
@@ -42,99 +43,14 @@ public class Draw extends JComponent{
 		}
 	}
 
-	public void reloadImage(){
-		state++;
+	
 
-		if(state == 0){
-			resource = getClass().getResource("run0.png");
-		}
-		else if(state == 1){
-			resource = getClass().getResource("run1.png");
-		}
-		else if(state == 2){
-			resource = getClass().getResource("run2.png");
-		}
-		else if(state == 3){
-			resource = getClass().getResource("run3.png");
-		}
-		else if(state == 4){
-			resource = getClass().getResource("run4.png");
-		}
-		else if(state == 5){
-			resource = getClass().getResource("run5.png");
-			state = 0;
-		}
-
-		try{
-			image = ImageIO.read(resource);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	public void attackAnimation(){
-		Thread thread1 = new Thread(new Runnable(){
-			public void run(){
-				for(int ctr = 0; ctr < 5; ctr++){
-					try {
-						if(ctr==4){
-							resource = getClass().getResource("run0.png");
-						}
-						else{
-							resource = getClass().getResource("attack"+ctr+".png");
-						}
-						
-						try{
-							image = ImageIO.read(resource);
-						}
-						catch(IOException e){
-							e.printStackTrace();
-						}
-				        repaint();
-				        Thread.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-		thread1.start();
-	}
-
-	public void attack(){
-		attackAnimation();
-	}
-
-	public void moveUp(){
-		y = y - 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveDown(){
-		y = y + 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveLeft(){
-		x = x - 5;
-		reloadImage();
-		repaint();
-	}
-
-	public void moveRight(){
-		x = x + 5;
-		reloadImage();
-		repaint();
-	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.YELLOW);
 		g.drawImage(backgroundImage, 0, 0, this);
-		g.drawImage(image, x, y, this);
+		g.drawImage(player.image, player.x, player.y, this);
 
 		g.drawImage(monster1.image, monster1.xPos, monster1.yPos, this);
 		g.drawImage(monster2.image, monster2.xPos, monster2.yPos, this);
